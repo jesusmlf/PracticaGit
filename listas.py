b1f706e3192f2adacda9b96219361c33b9c9cffa
Gzuz <jesusmlf7@gmail.com>
2022-06-22 15:01:56 -0500

    Agregamos una funcion que nos permite obtener la cantidad de alumnos y se corrigio el error en la funcion promedio

--- listas.py
+++ listas.py
@@ -1,12 +1,24 @@
+from faulthandler import cancel_dump_traceback_later
+
+
 def promedio(lista):
     contador = 0
     for i in lista:
         contador += i
-    return contador
+    return contador/len(lista)
+
+def entrada_usuario(cantidad_alumnos):
+    arr = []
+    for i in range(0,cantidad_alumnos):
+        calificacion = 0
+        calificacion = int(input("Ingrese la calificacion del alumno numero " + str(i+1) + ": "))
+        arr.append(calificacion)
+    return arr
 
 def run():
-    lista = [1,2,3,4,5]
-    print(promedio(lista))
+    cantidad_alumnos = int(input("Ingrese la cantidad de alumnos: "))
+    lista_alumnos= entrada_usuario(cantidad_alumnos)
+    print(promedio(lista_alumnos))
 
 if __name__ == '__main__':
     run()
