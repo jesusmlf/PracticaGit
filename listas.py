763c6da143b1122875bea955ee541c5db234f58f
Gzuz <jesusmlf7@gmail.com>
2022-06-22 14:52:41 -0500

    Agregando funcion "promedio"

--- listas.py
+++ listas.py
@@ -1,5 +1,12 @@
-def run:
-    pass
+def promedio(lista):
+    contador = 0
+    for i in lista:
+        contador += i
+    return contador
+
+def run():
+    lista = [1,2,3,4,5]
+    print(promedio(lista))
 
 if __name__ == '__main__':
     run()
