65145e51efc30c62b27d313c42009a32071607c8
Gzuz <jesusmlf7@gmail.com>
2022-07-02 13:41:32 -0500

    Agregamos el archivo 'suma.py'

---
+++ suma.py
@@ -0,0 +1,10 @@
+def run():
+    n1 = int(input("Ingrese el primer numero: "))
+    n2 = int(input("Ingrese el segundo numero: "))
+
+    suma = n1+n2
+    print(f"La suma es: {suma}")
+
+if __name__ == '__main__':
+    run()
+    
